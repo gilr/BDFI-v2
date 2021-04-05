792582665a7ba7b6199a93f32fba8f0b6bfdad85
Gil <gilles@bdfi.net>
2021-04-05 10:14:58 +0200

    Country table added

---
+++ app/Nova/Country.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Nova;
+
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Fields\Number;
+use Laravel\Nova\Fields\DateTime;
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+class Country extends Resource
+{
+    public static $model = \App\Models\Country::class;
+
+    /* Displayed field uses as title on detail pages */
+    public static $title = 'name';
+
+    /* The columns that could be searched. */
+    public static $search = [
+        'name', 'nationality', 'internal_order'
+    ];
+
+    /* Logical group in the sidebar menu - Optional */
+    public static $group = 'Internal Tables';
+
+    /* Label in the sidebar menu */
+    // Problématique : ça devient "Update pay" par exemple !
+    // public static function label () { return "Pays"; }
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function fields(Request $request)
+    {
+        return [
+            ID::make(__('(Base ID)'), 'id')
+                ->sortable(),
+            Number::make(__('Internal order'), 'internal_order')
+                ->rules('required', 'integer', 'gt:0')
+                ->sortable(),
+            Text::make(__('Name'), 'name')
+                ->rules('required', 'string', 'min:3')
+                ->creationRules('unique:countries,name')
+                ->updateRules('unique:countries,name,{{resourceId}}')
+                ->textAlign('center')
+                ->sortable(),
+            Text::make(__('Nationality'), 'nationality')
+                ->rules('required', 'string', 'min:3')
+                ->creationRules('unique:countries,nationality')
+                ->updateRules('unique:countries,nationality,{{resourceId}}')
+                ->sortable(),
+            Text::make(__('Code'), 'code')
+                ->rules('required', 'string', 'size:2')
+                ->creationRules('unique:countries,code')
+                ->updateRules('unique:countries,code,{{resourceId}}')
+                ->hideFromIndex(),
+            DateTime::make('Created At')
+                ->sortable()
+                ->format('DD/MM/YYYY HH:mm')
+                ->exceptOnForms(),
+            DateTime::make('Updated At')
+                ->format('DD/MM/YYYY HH:mm')
+                ->onlyOnDetail(),
+        ];
+    }
+
+    /* The visual style used for the table. Available options are 'tight' and 'default' */
+    public static $tableStyle = 'tight';
+
+    /* Whether to show borders for each column on the X-axis. */
+    public static $showColumnBorders = false;
+
+    /**
+     * Get the cards available for the request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function cards(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function filters(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function lenses(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function actions(Request $request)
+    {
+        return [];
+    }
+}
--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -15,6 +15,9 @@
                     <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-jet-nav-link>
+                    <x-jet-nav-link href="{{ route('nova.index') }}" :active="request()->routeIs('nova.index')">
+                        {{ __('Administration') }}
+                    </x-jet-nav-link>
                 </div>
             </div>
 
